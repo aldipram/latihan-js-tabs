9ec4012152f67fd73bd8693b71175c1f635de540
aldipram <nandaaldi80@gmail.com>
2023-10-28 08:37:18 +0700

    menjalankan tombol tabs

--- script/apps.js
+++ script/apps.js
@@ -1,3 +1,19 @@
-const about = document.querySelector(".about");
-const btns = document.querySelectorAll(".tab-btn");
-const articles = document.querySelectorAll(".content");
+const about = document.querySelector('.about')
+const btns = document.querySelectorAll('.tab-btn')
+const articles = document.querySelectorAll('.content')
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btns.forEach(btn => btn.classList.remove('active'));
+
+        // warna
+        btn.classList.add('active'); 
+        
+        articles.forEach(content => {
+            content.classList.remove('active');
+            if (content.getAttribute('id') === btn.getAttribute('data-id')) {
+                content.classList.add('active');
+            };
+        });
+    });
+});
